561cc50e9818b0fde3bbb307e33d18848a4a39bb
주해용 <jhy753@gmail.com>
2016-07-20 22:31:40 +0900

    view 컨트롤러 추가

---
+++ view.php
@@ -0,0 +1,15 @@
+<?php
+
+include "lib/include.php";
+
+$post = query(" select *from post where id=".$_GET['id'] );
+
+$vars = array(
+	'post'=> $post->fetch()
+);
+
+includeHeader();
+includeSkin("view.php", $vars );
+includeFooter();
+
+?>
